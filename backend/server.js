2933232086e4c832b8e19377e07c84be2f87df0b
Shivam Chaudhary <sk12022001@gmail.com>
2024-08-09 15:33:11 +0530

    Finish Setup MongoDB Connection

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+        res.send("Server Running!");
+
+});
+
+
+
+app.listen(5000, () => {
+    connectDB();
+    console.log("Server started at port http://localhost:5000");
+});
